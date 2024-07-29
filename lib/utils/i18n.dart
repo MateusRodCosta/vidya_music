df3828e11fc010f668ffe05abaf8ac9624c59865
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2024-07-29 17:15:40 -0300

    Rename "en" i18n file to "en-US"

--- lib/utils/i18n.dart
+++ lib/utils/i18n.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 import 'package:easy_localization/easy_localization.dart';
 
-const appSupportedLocalesAsString = ['en', 'pt_BR'];
+const appSupportedLocalesAsString = ['en_US', 'pt_BR'];
 List<Locale> get appSupportedLocales =>
     appSupportedLocalesAsString.map((s) => s.toLocale()).toList();
 Locale get appDefaultLocale => appSupportedLocales.first;
