d36ed1361bfff3238557fd70ae0fa848595d95a9
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2024-10-12 11:02:38 -0300

    Upgrade Gradle, AGP, Kotlin and Java
    Java: 17 -> 21
    Gradle: 8.9 -> 8.10.2
    AGP: 8.5.1 -> 8.7.0
    Kotlin: 1.9.25 -> 2.0.21

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 android {
@@ -86,11 +86,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
