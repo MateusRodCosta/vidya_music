d493acaa88114af90d91108ae05da88a51989fa7
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2025-02-06 18:44:44 -0300

    Set NDK options: supported arches and debug symbols

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,6 +38,11 @@ android {
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        ndk {
+            abiFilters.addAll(arrayOf("armeabi-v7a", "arm64-v8a", "x86_64"))
+            debugSymbolLevel = "FULL"
+        }
     }
 
     signingConfigs {
