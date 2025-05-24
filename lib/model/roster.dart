a7fb49a5e165272ce182943175ae96c04a64588d
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2025-05-24 16:13:51 -0300

    Refactor Roster.fromJson constructor to initializer list

--- lib/model/roster.dart
+++ lib/model/roster.dart
@@ -10,29 +10,20 @@ class Roster extends Equatable {
     this.newId,
   });
 
-  factory Roster.fromJson(Map<String, dynamic> json, {bool getSource = false}) {
-    final changelog = json['changelog'] as String;
-    final url = json['url'] as String;
-    final ext = json['ext'] as String;
-    final newId = json['new_id'] as int?;
-    final tracks =
-        (json['tracks'] as List<dynamic>)
-            .map(
-              (t) => Track.fromJson(
-                t as Map<String, dynamic>,
-                getSource: getSource,
-              ),
-            )
-            .toList();
-
-    return Roster(
-      changelog: changelog,
-      url: url,
-      ext: ext,
-      tracks: tracks,
-      newId: newId,
-    );
-  }
+  Roster.fromJson(Map<String, dynamic> json, {bool getSource = false})
+    : changelog = json['changelog'] as String,
+      url = json['url'] as String,
+      ext = json['ext'] as String,
+      tracks =
+          (json['tracks'] as List<dynamic>)
+              .map(
+                (t) => Track.fromJson(
+                  t as Map<String, dynamic>,
+                  getSource: getSource,
+                ),
+              )
+              .toList(),
+      newId = json['new_id'] as int?;
 
   final String changelog;
   final String url;
@@ -41,7 +32,7 @@ class Roster extends Equatable {
   final int? newId;
 
   @override
-  List<Object?> get props => [changelog, url, ext, newId, tracks];
+  List<Object?> get props => [changelog, url, ext, tracks, newId];
 
   @override
   bool get stringify => true;
