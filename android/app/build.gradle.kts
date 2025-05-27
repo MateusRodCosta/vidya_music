b57adb2aaea85ea7a9b6fb9fdfd3f2eb59f98e49
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2025-05-27 15:15:43 -0300

    Conditionally disable split abis when building app bundles
    This is needed due to a bug introduced in AGP 8.9.0: https://issuetracker.google.com/issues/402800800

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -90,7 +90,16 @@ android {
 
     splits {
         abi {
-            isEnable = true
+            // Detect app bundle and conditionally disable split abis
+            // This is needed due to a "Sequence contains more than one matching element" error
+            // present since AGP 8.9.0, for more info see:
+            // https://issuetracker.google.com/issues/402800800
+
+            // AppBundle tasks usually contain "bundle" in their name
+            val isBuildingBundle = gradle.startParameter.taskNames.any { it.lowercase().contains("bundle") }
+
+            // Disable split abis when building appBundle
+            isEnable = !isBuildingBundle
 
             reset()
             //noinspection ChromeOsAbiSupport
